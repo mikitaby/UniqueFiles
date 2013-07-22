1114be1cb0be50d6ebfa3abc45ece2489ee1511d
mikitaby <mikita_by@tut.by>
2013-07-22 15:20:20 +0300

    move logger outside from engine

--- src/controller/Loader.java
+++ src/controller/Loader.java
@@ -4,11 +4,18 @@ import java.io.File;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 class Loader {
+	private static final Logger log = LogManager.getLogger(Loader.class);
+	
 	public static void main(String[] args) {
 		
 		FolderHelper folderHelper = new FolderHelper("d:\\1\\");
+		log.info("Start analysing...");
 		Collection<List<File>> files = SameFilesEngine.analyse(folderHelper.getListFiles("d:\\1\\"));
+		log.info("Finish analysing");
 		StatHelper.show(files);		
 		//folderHelper.move(files);
 	}
--- src/controller/SameFilesEngine.java
+++ src/controller/SameFilesEngine.java
@@ -7,29 +7,22 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import model.FileKey;
 
 public class SameFilesEngine {
-	private static final Logger log = LogManager.getLogger(SameFilesEngine.class);
 
 	public static Collection<List<File>> analyse(List<File> files) {
-		log.info("Start analysing...");
 		Map<FileKey, List<File>> hm = new HashMap<>();
 		for (File file : files) {
 			FileKey key = new FileKey(file);
 			if (hm.containsKey(key)) {
 				hm.get(key).add(file);
 			} else {
-				LinkedList<File> sameFiles = new LinkedList<>();
+				List<File> sameFiles = new LinkedList<>();
 				sameFiles.add(file);
 				hm.put(key, sameFiles);
 			}
 		}
-
-		log.info("Finish analysing");
 		return hm.values();
-	}	
+	}
 }
